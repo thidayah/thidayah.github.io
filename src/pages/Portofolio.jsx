c30e97dfbd60e291e4f4d9d9a111ec750fc143ef
taufik <taufik@codelabs.co.id>
2025-05-28 11:29:00 +0700

    feat (fortofolio): set list data of fortofolio

--- src/pages/Portofolio.jsx
+++ src/pages/Portofolio.jsx
@@ -2,30 +2,180 @@ import React from 'react'
 import { useState } from "react"
 
 // Portofolio
-import imageProject1 from './../assets/project-1.jpg'
-import imageProject2 from './../assets/project-2.jpg'
-import imageProject3 from './../assets/project-3.jpg'
+// import imageProject1 from './../assets/project-1.jpg'
+// import imageProject2 from './../assets/project-2.jpg'
+// import imageProject3 from './../assets/project-3.jpg'
+import imageKlaklik from './../assets/Klaklik.png'
+import imageHRDMobile from './../assets/HRDMobile.png'
+import imageCityTours from './../assets/CityTours.png'
+import imagePrivilegez from './../assets/Privilegez.png'
+import imageYubiChat from './../assets/YubiChat.png'
+import imageSIL from './../assets/SIL.png'
+import imageCiptaKarya from './../assets/CiptaKarya.png'
+import imageBMBK from './../assets/BMBK.png'
+import imageEMA from './../assets/EMA.png'
+import imageTourizpass from './../assets/Tourizpass.png'
+import imageEdot from './../assets/Edot.png'
+import imageRuangPasar from './../assets/RuangPasar.png'
+import imageEzview from './../assets/Ezview.png'
+import imageEzrx from './../assets/Ezrx.png'
+import imageLNG from './../assets/LNG.png'
+import imagePaseeroWeb from './../assets/PaseeroWeb.png'
+import imagePaseeroMobile from './../assets/PaseeroMobile.png'
+import imageKNS from './../assets/KNS.png'
+import imageNex from './../assets/Nex.png'
+import imageBearny from './../assets/Bearny.png'
+import imageDailyTax from './../assets/DailyTax.png'
 
 const Portofolio = () => {
   const [filterGroup, setFilterGroup] = useState('')
   const projects = [
     {
       id: 1,
-      name: 'Finance',
+      name: 'Klaklik',
       type: 'Web Development',
-      image: imageProject1,
+      image: imageKlaklik,
+      url: 'https://www.klaklik.com/'
+    },
+    {
+      id: 111,
+      name: 'HRD Mobile',
+      type: 'Applications',
+      image: imageHRDMobile,
+      url: '#'
     },
     {
       id: 2,
-      name: 'Orizon',
+      name: 'CityTours',
       type: 'Web Development',
-      image: imageProject2,
+      image: imageCityTours,
+      url: 'https://m.citytours.sg/'
     },
     {
       id: 3,
-      name: 'Fundo',
+      name: 'Privilegez',
+      type: 'Web Development',
+      image: imagePrivilegez,
+      url: '#'
+    },
+    {
+      id: 331,
+      name: 'Yubi Chat',
       type: 'Applications',
-      image: imageProject3,
+      image: imageYubiChat,
+      url: '#'
+    },
+    {
+      id: 4,
+      name: 'SIL Tangerang',
+      type: 'Web Development',
+      image: imageSIL,
+      url: 'https://sil.tangerangkota.go.id/'
+    },
+    {
+      id: 5,
+      name: 'Cipta Karya Tata Ruang',
+      type: 'Web Development',
+      image: imageCiptaKarya,
+      url: 'https://dataciptakarya.tangerangkota.go.id/'
+    },
+    {
+      id: 6,
+      name: 'BMBK',
+      type: 'Web Development',
+      image: imageBMBK,
+      url: 'https://dinasbmbk.lampungprov.go.id/'
+    },
+    {
+      id: 7,
+      name: 'Event Management',
+      type: 'Web Development',
+      image: imageEMA,
+      url: '#'
+    },
+    {
+      id: 8,
+      name: 'Tourizpass',
+      type: 'Web Development',
+      image: imageTourizpass,
+      url: '#'
+    },
+    {
+      id: 9,
+      name: 'Ruang Pasar',
+      type: 'Web Development',
+      image: imageRuangPasar,
+      url: '#'
+    },
+    {
+      id: 991,
+      name: 'eDot Socmed',
+      type: 'Applications',
+      image: imageEdot,
+      url: '#'
+    },
+    {
+      id: 10,
+      name: 'Ezview',
+      type: 'Web Development',
+      image: imageEzview,
+      url: 'https://ezview.id/'
+    },
+    {
+      id: 11,
+      name: 'Ezrx',
+      type: 'Web Development',
+      image: imageEzrx,
+      url: 'https://ezrx.id/'
+    },
+    {
+      id: 12,
+      name: 'LNG',
+      type: 'Web Development',
+      image: imageLNG,
+      url: 'https://dev.indolng.co.id/'
+    },
+    {
+      id: 13,
+      name: 'Passero Web',
+      type: 'Web Development',
+      image: imagePaseeroWeb,
+      url: 'https://paseero.ng/'
+    },
+    {
+      id: 14,
+      name: 'Passero Mobile',
+      type: 'Applications',
+      image: imagePaseeroMobile,
+      url: 'https://play.google.com/store/apps/details?id=com.paseerorn&pcampaignid=web_share'
+    },
+    {
+      id: 15,
+      name: 'KNS',
+      type: 'Web Development',
+      image: imageKNS,
+      url: 'https://kns.asia/'
+    },
+    {
+      id: 15151,
+      name: 'Nex Security',
+      type: 'Applications',
+      image: imageNex,
+      url: '#'
+    },
+    {
+      id: 15152,
+      name: 'Bearny',
+      type: 'Applications',
+      image: imageBearny,
+      url: '#'
+    },
+    {
+      id: 16,
+      name: 'Daily Tax',
+      type: 'Web Development',
+      image: imageDailyTax,
+      url: '#'
     },
   ]
 
@@ -41,7 +191,7 @@ const Portofolio = () => {
         {(filterGroup !== '' ? projects.filter(f => f.type === filterGroup) : projects).map((row) => (
           <div >
             <img
-              className=" rounded-xl"
+              className=" rounded-xl transition-all hover:scale-125 duration-300"
               src={row?.image}
               alt={row?.name}
             />
