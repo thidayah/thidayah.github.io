9e82a6ad3455de0af22c3f9a117c5c380c463ebd
taufik <taufik@codelabs.co.id>
2025-06-05 14:12:38 +0700

    update: responsive page resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,30 +6,30 @@ const Resume = () => {
     <>
       <div className=" mt-8">
         <div className=" flex items-center space-x-6">
-          <div className=" size-12 rounded-xl shadow shadow-[#ffdb70] flex justify-center items-center">
-            <HiOutlineBookOpen className=" text-[#ffdb70] size-6" />
+          <div className=" size-10 md:size-12 rounded-xl shadow shadow-[#ffdb70] flex justify-center items-center">
+            <HiOutlineBookOpen className=" text-[#ffdb70] size-4 md:size-6" />
           </div>
-          <div className="text-3xl font-bold text-white">Education</div>
+          <div className="text-xl md:text-3xl font-bold text-white">Education</div>
         </div>
-        <div className="mt-6 ml-4 space-y-4">
-          <div className=" flex space-x-10 ">
+        <div className="mt-6 ml-2 md:ml-4 space-y-4">
+          <div className=" flex space-x-8 md:space-x-10 ">
             <div>
               <div className=" size-5 rounded-full bg-[#fecb66] border-4 border-[#383838] mt-1 " />
             </div>
             <div>
-              <div className=" text-[#fafafa] text-xl font-semibold">SMK TI Pembangunan</div>
-              <div className=" text-[#ceb15a] text-lg">2011 — 2014</div>
-              <div className=" text-[#d6d6d6] text-lg">Software Engineering</div>
+              <div className=" text-[#fafafa] md:text-xl font-semibold">SMK TI Pembangunan</div>
+              <div className=" text-[#ceb15a] text-sm md:text-lg">2011 — 2014</div>
+              <div className=" text-[#d6d6d6] text-sm md:text-lg">Software Engineering</div>
             </div>
           </div>
-          <div className=" flex space-x-10 ">
+          <div className=" flex space-x-8 md:space-x-10 ">
             <div>
               <div className=" size-5 rounded-full bg-[#fecb66] border-4 border-[#383838] mt-1 " />
             </div>
             <div>
-              <div className=" text-[#fafafa] text-xl font-semibold">STMIK Indonesia Mandiri</div>
-              <div className=" text-[#ceb15a] text-lg">2017 — 2021</div>
-              <div className=" text-[#d6d6d6] text-lg">Informatics Engineering</div>
+              <div className=" text-[#fafafa] md:text-xl font-semibold">STMIK Indonesia Mandiri</div>
+              <div className=" text-[#ceb15a] text-sm md:text-lg">2017 — 2021</div>
+              <div className=" text-[#d6d6d6] text-sm md:text-lg">Informatics Engineering</div>
             </div>
           </div>
         </div>
@@ -37,50 +37,50 @@ const Resume = () => {
 
       <div className=" mt-8">
         <div className=" flex items-center space-x-6">
-          <div className=" size-12 rounded-xl shadow shadow-[#ffdb70] flex justify-center items-center">
-            <HiOutlineCodeBracket className=" text-[#ffdb70] size-6" />
+          <div className=" size-10 md:size-12 rounded-xl shadow shadow-[#ffdb70] flex justify-center items-center">
+            <HiOutlineCodeBracket className=" text-[#ffdb70] size-4 md:size-6" />
           </div>
-          <div className="text-3xl font-bold text-white">Experience</div>
+          <div className="text-xl md:text-3xl font-bold text-white">Experience</div>
         </div>
-        <div className="mt-6 ml-4 space-y-4">
-          <div className=" flex space-x-10 ">
+        <div className="mt-6 ml-2 md:ml-4 space-y-4">
+          <div className=" flex space-x-8 md:space-x-10 ">
             <div>
               <div className=" size-5 rounded-full bg-[#fecb66] border-4 border-[#383838] mt-1 " />
             </div>
             <div>
-              <div className=" text-[#fafafa] text-xl font-semibold">Web Programmer</div>
-              <div className=" text-[#ceb15a] text-lg">2016 — 2017</div>
-              <div className=" text-[#d6d6d6] text-lg">at Nusantara Citra</div>
+              <div className=" text-[#fafafa] md:text-xl font-semibold">Web Programmer</div>
+              <div className=" text-[#ceb15a] text-sm md:text-lg">2016 — 2017</div>
+              <div className=" text-[#d6d6d6] text-sm md:text-lg">at Nusantara Citra</div>
             </div>
           </div>
-          <div className=" flex space-x-10 ">
+          <div className=" flex space-x-8 md:space-x-10 ">
             <div>
               <div className=" size-5 rounded-full bg-[#fecb66] border-4 border-[#383838] mt-1 " />
             </div>
             <div>
-              <div className=" text-[#fafafa] text-xl font-semibold">Backend Developer</div>
-              <div className=" text-[#ceb15a] text-lg">2018 — 2020</div>
-              <div className=" text-[#d6d6d6] text-lg">at Codelabs Indonesia</div>
+              <div className=" text-[#fafafa] md:text-xl font-semibold">Backend Developer</div>
+              <div className=" text-[#ceb15a] text-sm md:text-lg">2018 — 2020</div>
+              <div className=" text-[#d6d6d6] text-sm md:text-lg">at Codelabs Indonesia</div>
             </div>
           </div>
-          <div className=" flex space-x-10 ">
+          <div className=" flex space-x-8 md:space-x-10 ">
             <div>
               <div className=" size-5 rounded-full bg-[#fecb66] border-4 border-[#383838] mt-1 " />
             </div>
             <div>
-              <div className=" text-[#fafafa] text-xl font-semibold">Fronted Developer</div>
-              <div className=" text-[#ceb15a] text-lg">2020 — Now</div>
-              <div className=" text-[#d6d6d6] text-lg">at Codelabs Indonesia</div>
+              <div className=" text-[#fafafa] md:text-xl font-semibold">Fronted Developer</div>
+              <div className=" text-[#ceb15a] text-sm md:text-lg">2020 — Now</div>
+              <div className=" text-[#d6d6d6] text-sm md:text-lg">at Codelabs Indonesia</div>
             </div>
           </div>
-          <div className=" flex space-x-10 ">
+          <div className=" flex space-x-8 md:space-x-10 ">
             <div>
               <div className=" size-5 rounded-full bg-[#fecb66] border-4 border-[#383838] mt-1 " />
             </div>
             <div>
               <div className=" text-[#fafafa] text-xl font-semibold">Mobile Developer</div>
-              <div className=" text-[#ceb15a] text-lg">2022 — Now</div>
-              <div className=" text-[#d6d6d6] text-lg">at Codelabs Indonesia</div>
+              <div className=" text-[#ceb15a] text-sm md:text-lg">2022 — Now</div>
+              <div className=" text-[#d6d6d6] text-sm md:text-lg">at Codelabs Indonesia</div>
             </div>
           </div>
         </div>
